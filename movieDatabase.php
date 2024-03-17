a69f017c5b63269bb166f2d260ba81e79b09895e
IshalImran <163378680+IshalImran@users.noreply.github.com>
2024-03-17 21:01:26 +0500

    Add files via upload

---
+++ movieDatabase.php
@@ -0,0 +1,14 @@
+<?php
+$host = 'yazureprivatedns.net';
+$dbname = 'HungamaMovies_group';
+$username = 'Ishal Imran';
+$password = '9zSXnY+g89e-Df8';
+
+try {
+    $conn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+    // Set PDO to throw exceptions on errors
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch(PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+}
+?>
